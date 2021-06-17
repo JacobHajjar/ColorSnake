64009b649c51b708318a91a6603a1e95962dde4a
JacobHajjar <jacob.hajjar1@gmail.com>
2021-06-17 00:35:09 -0700

    First save, using namedTuple

--- snake.py
+++ snake.py
@@ -1,42 +1,55 @@
-import pygame, sys
+#!/usr/bin/env python3
+'''My own remake of the classic video game Snake'''
+import random
+import pygame
+import sys
 from pygame.locals import *
+from collections import namedtuple
 
+__author__ = 'Jacob Hajjar'
+__email__ = 'hajjarj@csu.fullerton.edu'
+__maintainer__ = 'jacobhajjar'
+
+Colors = namedtuple('Colors', ['black', 'white'])
+Settings = namedtuple('Settings', ['FPS', 'window_width', 'window_height',
+                                   'box_size', 'menu_color', 'bg_color', 'margin_size'])
+colors = Colors((0,   0,   0), (255, 255, 255))
+settings = Settings(12, 640, 480, 20, colors.white, colors.white, 20)
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((500,400), 0, 32)
+fps_clock = pygame.time.Clock()
+display_surf = pygame.display.set_mode(
+    (settings.window_width, settings.window_height))
+title_font = pygame.font.Font('title_font.otf', 80)
+title_render = title_font.render('SNAKE', False, colors.black, None)
 
-pygame.display.set_caption('Drawing')
+def main():
+    # main function for the snake game
 
-#set up the colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+    while True:  # main menu selections loop
+        in_game = False
+        display_menu()
+        while True:  # game started loop
+            for event in pygame.event.get():  # event handling loop
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+            mousex, mousey = pygame.mouse.get_pos()
+            if not in_game:
+                display_surf.blit(title_render, [200, 200])
+            pygame.display.update()
+            fps_clock.tick(settings.FPS)
+        break
 
-#draw the surface object
-DISPLAYSURF.fill(WHITE)
-pygame.draw.polygon(DISPLAYSURF, GREEN, ((146, 0), (291, 106), (236, 277), (56, 277), 
-(0, 106)))
-pygame.draw.line(DISPLAYSURF, BLUE, (60, 60), (120, 60), 4)
-pygame.draw.line(DISPLAYSURF, BLUE, (120, 60), (60, 120))
-pygame.draw.line(DISPLAYSURF, BLUE, (60, 120), (120, 120), 4)
-pygame.draw.circle(DISPLAYSURF, BLUE, (300, 50), 20, 0)
-pygame.draw.ellipse(DISPLAYSURF, RED, (300, 250, 40, 80), 1)
-pygame.draw.rect(DISPLAYSURF, RED, (200, 150, 100, 50))
 
-pixObj = pygame.PixelArray(DISPLAYSURF)
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-    myColor = pygame.Color(255, 0, 0)
-    spamRect = pygame.Rect(10, 20, 200, 300)
-    spamRect == (10, 20, 200, 300)
-    pygame.display.update()
+def display_menu():
+    '''function which runs the game's main menu'''
+    display_surf.fill(settings.menu_color)
+    text_box = title_render.get_rect()
+    text_box.center = (200, 150)
 
-'''def main():
-    print()
+def play_game():
+    '''function which runs the main snake game'''
+
 
 if __name__ == '__main__':
-    main()'''
+    main()
