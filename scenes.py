deb629cc19fe67f065e1ec255683db95b8908e4e
JacobHajjar <jacob.hajjar1@gmail.com>
2021-06-21 22:32:06 -0700

    technically a merge, corrupted branch fix

---
+++ scenes.py
@@ -0,0 +1,163 @@
+'''Class containing all of the game's scenes'''
+import random
+import sys
+import pygame
+from pygame.locals import *
+pygame.init()
+
+
+class MenuScene:
+    '''class for the menu scenes of the game'''
+    in_scene = True
+    curr_mouse = [-1, -1]
+    curr_direction = 'up'
+    next_scene = 0
+
+    def __init__(self, colors, display_surf):
+        self.colors = colors
+        self.display_surf = display_surf
+
+    def start_scene(self):
+        '''runs the scene in the game'''
+        fps = 30
+        self.in_scene = True
+        self.display_surf.fill(self.colors.white)
+        fps_clock = pygame.time.Clock()
+        while self.in_scene:  # game started loop
+            self.curr_mouse = [-1, -1]
+            self.curr_direction = None
+            for event in pygame.event.get():  # event handling loop
+                #pylint: disable=E0602
+                # ^ pylint doesn't like pygame event variables :(
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == MOUSEBUTTONDOWN:
+                    mousex, mousey = event.pos
+                    self.curr_mouse = [mousex, mousey]
+                elif event.type == KEYDOWN:
+                    if event.key in (K_LEFT, K_a):
+                        self.curr_direction = 'left'
+                    elif event.key in (K_RIGHT, K_d):
+                        self.curr_direction = 'right'
+                    elif event.key in (K_UP, K_w):
+                        self.curr_direction = 'up'
+                    elif event.key in (K_DOWN, K_s):
+                        self.curr_direction = 'down'
+            self.display_scene()
+            pygame.display.update()
+            fps_clock.tick(fps)
+
+    def display_scene(self):
+        '''the logic and objects displayed in the scene'''
+        width, height = self.display_surf.get_size()
+        row_height = height * 5 / 6
+        menu_button_size = (160, 40)
+
+        # gray box for decoration
+        text_rend, text_box = load_text(
+            'title_font.otf', 120, self.colors.black, 'SNAKE')
+        self.display_centered_text(text_rend, text_box, (width/2, height/3))
+
+        self.display_centered_rect(
+            (width, 70), self.colors.lgray, 0, (width/2, row_height))
+        # start button
+        start_clicked = self.display_button_click(
+            menu_button_size, (width / 2, row_height), self.colors.red, self.colors.black)
+        text_rend, text_box = load_text(
+            'game_over.ttf', 70, self.colors.black, 'PLAY')
+        self.display_centered_text(
+            text_rend, text_box, (width / 2, row_height))
+        # game difficulty button
+        difficulty_clicked = self.display_button_click(
+            menu_button_size, (width / 5, row_height), self.colors.lime, self.colors.black)
+        text_rend, text_box = load_text(
+            'game_over.ttf', 70, self.colors.black, 'DIFFICULTY')
+        self.display_centered_text(
+            text_rend, text_box, (width / 5, row_height))
+        # highscore button
+        highscore_clicked = self.display_button_click(
+            menu_button_size, (width * 4 / 5, row_height), self.colors.aqua, self.colors.black)
+        text_rend, text_box = load_text(
+            'game_over.ttf', 70, self.colors.black, 'HIGHSCORE')
+        self.display_centered_text(
+            text_rend, text_box, (width * 4 / 5, row_height))
+
+        if start_clicked:
+            self.next_scene = 1
+            self.in_scene = False
+        elif difficulty_clicked:
+            print("Difficulty Selected")
+        elif highscore_clicked:
+            print("button3")
+
+    def display_centered_text(self, text_rend, text_box, xycenter_position):
+        '''easy function for displaying external text centered'''
+        text_box.center = xycenter_position
+        self.display_surf.blit(text_rend, text_box)
+
+    def display_left_text(self, text_rend, text_box, xyleft_position):
+        '''easy function for displaying text centered left'''
+        text_box.center = xyleft_position
+        self.display_surf.blit(text_rend, text_box)
+
+    def display_centered_rect(self, xy_size, col, outline_size, xycenter_position):
+        '''function for drawing a rectangle centered with coordinates'''
+        b_width = xy_size[0]
+        b_height = xy_size[1]
+        rect_obj = pygame.Rect(0, 0, b_width, b_height)
+        rect_obj.center = xycenter_position
+        pygame.draw.rect(self.display_surf, col, rect_obj, outline_size)
+        return rect_obj
+
+    def display_button_click(self, xy_size, xycenter_position, b_color, o_color):
+        '''function for displaying a button that can return a true or false if clicked'''
+        o_size = 3
+        mousex, mousey = pygame.mouse.get_pos()
+        start_button = self.display_centered_rect(
+            xy_size, b_color, 0, xycenter_position)
+        if start_button.collidepoint(mousex, mousey):
+            self.display_centered_rect(
+                xy_size, o_color, o_size, xycenter_position)
+        if start_button.collidepoint(self.curr_mouse[0], self.curr_mouse[1]):
+            return True
+        return False
+
+
+class SnakeScene(MenuScene):
+    '''scene for the main snake game'''
+    snake_grid = []
+    margin = 40
+    box_size = 20
+
+    def display_scene(self):
+        self.display_surf.fill(self.colors.black)
+        width, height = self.display_surf.get_size()
+
+        play_area = self.display_centered_rect(
+            (width-self.margin * 2, height-self.margin * 2), self.colors.white, 2, (width/2, height/2))
+        self.draw_snake_grid()
+
+    def draw_snake_grid(self):
+        '''draws the game grid for snake'''
+        width, height = self.display_surf.get_size()
+        margin = self.margin
+        box_size = self.box_size
+
+        for x_val in range(margin, width-margin, box_size):
+            row = []
+            for y_val in range(margin, height-margin, box_size):
+                grid_box = pygame.Rect(x_val, y_val, box_size, box_size)
+                row.append(grid_box)
+
+                pygame.draw.rect(self.display_surf,self.colors.white, grid_box, 2)
+            print(row)
+            self.snake_grid.append(row)
+
+
+def load_text(font, size, col, msg):
+    '''function to help load text'''
+    font_obj = pygame.font.Font(font, size)
+    font_render = font_obj.render(msg, True, col, None)
+    #render(text, antialias, color, background=None)
+    return font_render, font_render.get_rect()
--- snake.py
+++ snake.py
@@ -1,54 +1,46 @@
 #!/usr/bin/env python3
-'''My own remake of the classic video game Snake'''
-import random
-import pygame
-import sys
-from pygame.locals import *
+'''My own remake of the classic video game Snake in Pygame'''
 from collections import namedtuple
+import pygame
+from scenes import MenuScene, SnakeScene
+pygame.init()
 
 __author__ = 'Jacob Hajjar'
 __email__ = 'hajjarj@csu.fullerton.edu'
 __maintainer__ = 'jacobhajjar'
 
-Colors = namedtuple('Colors', ['black', 'white'])
-Settings = namedtuple('Settings', ['FPS', 'window_width', 'window_height',
-                                   'box_size', 'menu_color', 'bg_color', 'margin_size'])
-colors = Colors((0,   0,   0), (255, 255, 255))
-settings = Settings(12, 640, 480, 20, colors.white, colors.white, 20)
-pygame.init()
-fps_clock = pygame.time.Clock()
-display_surf = pygame.display.set_mode(
-    (settings.window_width, settings.window_height))
-title_font = pygame.font.Font('title_font.otf', 80)
-title_render = title_font.render('SNAKE', False, colors.black, None)
+Colors = namedtuple(
+    'Colors', ['black', 'lgray', 'white', 'red', 'yellow', 'blue', 'lime', 'aqua', 'purple'])
+
+
+class SnakeGame:
+    '''class for the main logic of the program'''
+    colors = Colors((0, 0, 0), (100, 100, 100), (255, 255, 255), (255,   0,   0), (255, 255,   0),
+                    (0,  0, 255), (0, 255,   0), (0, 255, 255), (128,  0, 128))
+    window_width = 640
+    window_height = 480
+    display_surf = pygame.display.set_mode((window_width, window_height))
+    title = 'SNAKE'
+    next_scene = 0
+    pygame.display.set_caption(title)
+
+    def play_snake(self):
+        '''function which plays a round of snake'''
+        menu = MenuScene(self.colors, self.display_surf)
+        game_scene = SnakeScene(self.colors, self.display_surf)
+        scenes = [menu, game_scene]
+        while True:
+            scenes[self.next_scene].start_scene()
+            self.next_scene = scenes[self.next_scene].next_scene
+            print("SCENE CHANGED!!")
+
+
 
 def main():
-    # main function for the snake game
+    '''the main function to run the game'''
+    game1 = SnakeGame()
+    game1.play_snake()
 
-    while True:  # main menu selections loop
-        in_game = False
-        display_menu()
-        while True:  # game started loop
-            for event in pygame.event.get():  # event handling loop
-                if event.type == QUIT:
-                    pygame.quit()
-                    sys.exit()
-            mousex, mousey = pygame.mouse.get_pos()
-            if not in_game:
-                display_surf.blit(title_render, [200, 200])
-            pygame.display.update()
-            fps_clock.tick(settings.FPS)
-        break
-
-
-def display_menu():
-    '''function which runs the game's main menu'''
-    display_surf.fill(settings.menu_color)
-    text_box = title_render.get_rect()
-    text_box.center = (200, 150)
-
-def play_game():
-    '''function which runs the main snake game'''
 
 
 if __name__ == '__main__':
